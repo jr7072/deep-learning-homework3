188e13e4042e01e287bb7e04e0ad26de3d391b18
Juan Rosales <juanrosales@Juans-MacBook-Pro.local>
2026-03-27 08:18:23 -0500

    removed encoder and decoder dropout layers

--- homework/models.py
+++ homework/models.py
@@ -384,7 +384,7 @@ class Detector(torch.nn.Module):
             include_dropout = False
 
             if layer == (encoding_layers - 1):
-                include_dropout = True
+                include_dropout = False
 
             self.encoder_layers.append(
                 EncoderBlock(
@@ -423,7 +423,7 @@ class Detector(torch.nn.Module):
             include_dropout = False
 
             if i == (encoding_layers - 1):
-                include_dropout = True
+                include_dropout = False
 
             first_decode_layer = ConvBlock(
                 current_output_size,
